512209dc52300b7aacc91a39bac906561fd5dc29
Koji Sato <utautaouka@gmail.com>
2023-05-05 13:17:11 +0900

    event store

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreEventRequest;
 use App\Http\Requests\UpdateEventRequest;
+use App\Http\Services\EventService;
 use App\Models\Event;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class EventController extends Controller
 {
@@ -40,7 +42,35 @@ class EventController extends Controller
      */
     public function store(StoreEventRequest $request)
     {
-        //
+        $check = EventService::checkEventDuplocation(
+            $request['event_date'],
+            $request['start_time'],
+            $request['end_time']
+        );
+        if($check) {
+            session()->flash('status', 'この時間帯はすでに予約されています');
+            return view('manager.events.create');
+        }
+
+        $startDate = EventService::joinDateAndTime(
+            $request['event_date'],
+            $request['start_time']
+        );
+        $endDate = EventService::joinDateAndTime(
+            $request['event_date'],
+            $request['end_time']
+        );
+        Event::create([
+            'name' => $request['event_name'],
+            'information' => $request['information'],
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'max_people' => $request['max_people'],
+            'is_visible' => $request['is_visible'],
+        ]);
+
+        session()->flash('status', '登録しました');
+        return to_route('events.index');
     }
 
     /**
