d67d4830d820f5a72d259605899da45b2c6e3a0e
Koji Sato <utautaouka@gmail.com>
2023-05-11 21:51:11 +0900

    イベント計算

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -30,8 +30,8 @@ class EventFactory extends Factory
             'name' => $this->faker->name,
             'information' => $this->faker->realText,
             'max_people' => $this->faker->numberBetween(1, 20),
-            'start_date' => $startDate->format('Y-m-d H:i:s'),
-            'end_date' => $endDate->modify('+1hour')->format('Y-m-d H:i:s'),
+            'start_date' => $startDate,
+            'end_date' => $endDate,
             'is_visible' => $this->faker->boolean,
         ];
     }
