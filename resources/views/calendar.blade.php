1e35954f06275d8f2ed40476775c01ee2ad9a3f4
Koji Sato <utautaouka@gmail.com>
2023-05-14 13:01:02 +0900

    event reserve

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -8,7 +8,6 @@
     <div class="py-4">
         <div class="event-calendar border border-red-400 mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-
                 @livewire('calendar')
             </div>
         </div>
